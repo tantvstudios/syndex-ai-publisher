870be01102427fe012eb9251807a6a33f09def48
Paul Olajide <paulolajide.dev@gmail.com>
2025-10-29 23:58:14 +0100

    chore: sample services

--- src/config/axios-instance.ts
+++ src/config/axios-instance.ts
@@ -5,7 +5,8 @@ export const axiosInstance = axios.create({
 });
 
 axiosInstance.interceptors.request.use((config) => {
-	const token = localStorage.getItem("token");
+	const token =
+		"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1laWQiOiI0IiwidW5pcXVlX25hbWUiOiJ0by5vZGV5ZW1pQGdtYWlsLmNvbSIsIm5iZiI6MTc2MTc3NjA2NSwiZXhwIjoxNzYxNzc5NjY1LCJpYXQiOjE3NjE3NzYwNjV9.eh5rfBj58NB-D-G1vcUQCIzCApf0qhdW3GFBL7Exu6I";
 	if (token) {
 		config.headers.Authorization = `Bearer ${token}`;
 	}
