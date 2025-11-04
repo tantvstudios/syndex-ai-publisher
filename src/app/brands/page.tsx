89b524424bf3714a946ee7183b1a0e455e3a4c11
shresthamishra76 <shresthamishra76@gmail.com>
2025-11-03 19:08:25 -0600

    created basic leftSidebar with routes in /brands

---
+++ src/app/brands/page.tsx
@@ -0,0 +1,11 @@
+import { Flex } from "@chakra-ui/react";
+import { LeftSidebar } from "@source/components/ui/left-sidebar";
+const BrandsPage = () => {
+  return (
+    <Flex pt={"1rem"} pl={"1rem"}>
+      <LeftSidebar />
+    </Flex>
+  );
+};
+
+export default BrandsPage;
