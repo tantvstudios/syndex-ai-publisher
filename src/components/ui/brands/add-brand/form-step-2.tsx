b8b86e99cf869a06aece09c333514cd1efcebc5a
shresthamishra76 <shresthamishra76@gmail.com>
2025-11-14 12:18:47 -0600

    finished step 2

--- src/components/ui/brands/add-brand/form-step-2.tsx
+++ src/components/ui/brands/add-brand/form-step-2.tsx
@@ -1,4 +1,13 @@
-import { Box, Button, Dialog } from "@chakra-ui/react";
+import { Box, Button, Dialog, Text } from "@chakra-ui/react";
+import MultiSelectButton from "../../custom-multi-select-button";
+import { useState } from "react";
+
+const options = [
+  "Latino/Hispanic",
+  "Asian American",
+  "Native American",
+  "Black/African American",
+];
 
 export const Step2 = ({
   prevStep,
@@ -6,48 +15,95 @@ export const Step2 = ({
 }: {
   prevStep: () => void;
   nextStep: () => void;
-}) => (
-  <Box p={0}>
-    <Dialog.Header>STEP 2 (Step 2 of 2)</Dialog.Header>
-    <Dialog.Body>{/* Form Controls for Brand Name and RSS Feed */}</Dialog.Body>
-    <Dialog.Footer justifyContent="space-between" p={"2rem"}>
-      <Button
-        bg="white"
-        color="black"
-        borderColor={"black"}
-        variant="outline"
-        size="md"
-        width="45%"
-        rounded={"0.5rem"}
-        onClick={prevStep}
-      >
-        Back
-      </Button>
+}) => {
+  const [selected, setSelected] = useState<string[]>([]);
 
-      <Button
-        bg="black"
-        color="white"
-        _hover={{ bg: "gray.700" }}
-        size="md"
-        width="45%"
-        rounded={"0.5rem"}
-        onClick={nextStep}
+  return (
+    <Box p={0}>
+      <Dialog.Header
+        flex="column"
+        p={"2rem"}
+        justifyContent="center"
+        fontWeight="extrabold"
+        fontSize={"2rem"}
       >
-        Next
-      </Button>
-    </Dialog.Footer>
-    {/* <Dialog.Footer justifyContent="center" p={"2rem"}>
-      <Button
-        bg="black"
-        color="white"
-        _hover={{ bg: "gray.700" }}
-        size="md"
-        width="75%"
-        rounded={"0.5rem"}
-        onClick={nextStep}
-      >
-        Next
-      </Button>
-    </Dialog.Footer> */}
-  </Box>
-);
+        Create New Brand
+      </Dialog.Header>
+      <Dialog.Body>
+        <Box mb={12}>
+          <Text
+            as="label"
+            display="block"
+            mb={1}
+            fontSize="sm"
+            color={"#6d6d6d"}
+          >
+            Are you a local news outlet?
+          </Text>
+          <MultiSelectButton
+            options={["Yes", "No"]}
+            value={selected}
+            onChange={setSelected}
+            size="sm"
+            spacing={2}
+            maxSelections={1}
+          />
+          {selected.length > 0 && (
+            <Text mt={4} fontSize="sm" color="gray.600">
+              Selected: {selected.join(", ")}
+            </Text>
+          )}
+        </Box>
+        <Box mb={4}>
+          <Text
+            as="label"
+            display="block"
+            mb={1}
+            fontSize="sm"
+            color={"#6d6d6d"}
+          >
+            What community do you serve?
+          </Text>
+          <MultiSelectButton
+            options={options}
+            value={selected}
+            onChange={setSelected}
+            size="sm"
+            spacing={2}
+          />
+          {selected.length > 0 && (
+            <Text mt={4} fontSize="sm" color="gray.600">
+              Selected: {selected.join(", ")}
+            </Text>
+          )}
+        </Box>
+      </Dialog.Body>
+      <Dialog.Footer justifyContent="space-between" p={"2rem"}>
+        <Button
+          bg="white"
+          color="black"
+          borderColor={"black"}
+          variant="outline"
+          size="md"
+          width="45%"
+          rounded={"0.5rem"}
+          onClick={prevStep}
+        >
+          Back
+        </Button>
+
+        <Button
+          bg="black"
+          color="white"
+          _hover={{ bg: "gray.700" }}
+          size="md"
+          width="45%"
+          rounded={"0.5rem"}
+          onClick={nextStep}
+        >
+          Next
+        </Button>
+      </Dialog.Footer>
+    </Box>
+  );
+};
