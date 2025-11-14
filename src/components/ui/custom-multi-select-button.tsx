174e44f50c0bd4853ba72e45d1b6d4f216ca0a30
shresthamishra76 <shresthamishra76@gmail.com>
2025-11-14 12:09:20 -0600

    created custom multi-select button component

---
+++ src/components/ui/custom-multi-select-button.tsx
@@ -0,0 +1,57 @@
+import { Box, Button, HStack } from "@chakra-ui/react";
+import { FaPlus, FaMinus } from "react-icons/fa";
+
+interface MultiSelectButtonProps {
+  options: string[];
+  value?: string[];
+  onChange?: (selected: string[]) => void;
+  size?: "sm" | "md" | "lg";
+  spacing?: number;
+}
+
+interface value {
+  label: string;
+  value: any;
+}
+
+const MultiSelectButton = ({
+  options = [],
+  value = [],
+  onChange,
+  size = "md",
+  spacing = 2,
+}: MultiSelectButtonProps) => {
+  const toggle = (option: string) => {
+    const newSelected = value.includes(option)
+      ? value.filter((item) => item !== option)
+      : [...value, option];
+    onChange?.(newSelected);
+  };
+
+  return (
+    <Box>
+      <HStack wrap="wrap">
+        {options.map((opt) => {
+          const isSelected = value.includes(opt);
+          return (
+            <Button
+              key={opt}
+              onClick={() => toggle(opt)}
+              size={size}
+              bg={isSelected ? "#00f7ee" : "white"}
+              color={"black"}
+              borderColor="black"
+              borderWidth="1px"
+              cursor={"pointer"}
+            >
+              {isSelected ? <FaMinus /> : <FaPlus />}
+              {opt}
+            </Button>
+          );
+        })}
+      </HStack>
+    </Box>
+  );
+};
+
+export default MultiSelectButton;
